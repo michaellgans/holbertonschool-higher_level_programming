773a15989fba9b1dfa83d096d239e949de082305
michaellgans <michaellgans@hotmail.com>
2023-09-06 15:56:10 -0700

    test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
+    if number < 0:
+        number = number * -1
     x = number % 10
-    if x < 0:
-        x = x * -1
     print("{:d}".format(x), end="")
     return x
