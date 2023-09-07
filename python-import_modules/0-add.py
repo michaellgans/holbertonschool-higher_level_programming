9a5a25d77b54b86ee2d502901af5b29ef8ec52b3
michaellgans <michaellgans@hotmail.com>
2023-09-07 10:00:52 -0700

    added from

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
