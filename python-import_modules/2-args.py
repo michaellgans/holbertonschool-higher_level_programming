02142ede292e065254fa929076e9ab456014f49b
michaellgans <michaellgans@hotmail.com>
2023-09-07 20:18:07 -0700

    pycode style corrected?

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import sys #allows access to command line args
+
+import sys  # allows access to command line args
+
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("0 arguments.") #no arguments passed, only program name
+        print("0 arguments.")  # no arguments passed, only program name
     elif len(sys.argv) == 2:
-        print("1 argument:") #one argument passed
-        print("1: {}".format(sys.argv[1])) #print single argument
+        print("1 argument:")  # one argument passed
+        print("1: {}".format(sys.argv[1]))  # print single argument
     else:
-        print("{} arguments:".format(len(sys.argv) - 1)) #more than one arguments passed
+        print("{} arguments:".format(len(sys.argv) - 1))  # more than one args
         for x in range(1, len(sys.argv)):
-            print("{}: {}".format(x, sys.argv[x])) #print each argument
+            print("{}: {}".format(x, sys.argv[x]))  # print each argument
