6bf907ca8bc27b7114cb185649b4b1b2edfc6101
michaellgans <michaellgans@hotmail.com>
2023-09-11 14:48:12 -0700

    test

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
-        if  len(tuple_a) == 0:
+        if len(tuple_a) == 0:
             tuple_a = 0, 0
         else:
             tuple_a = tuple_a[0], 0
