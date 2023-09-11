c71318c7c6d969582d93a873471a7643ba199843
michaellgans <michaellgans@hotmail.com>
2023-09-11 12:27:11 -0700

    creates task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=" ")
+        print()
