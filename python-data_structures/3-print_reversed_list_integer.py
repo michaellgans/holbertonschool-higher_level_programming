53e7f11106b6f79c0c62060ccd499120abad7236
michaellgans <michaellgans@hotmail.com>
2023-09-11 09:56:43 -0700

    testing for empty list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
-        return my_list
+        print(my_list)
     for x in reversed(my_list):
         print("{:d}".format(x))
