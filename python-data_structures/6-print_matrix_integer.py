e3f46421cb416f1aa644f27c57cc6f9bbf1cd72e
michaellgans <michaellgans@hotmail.com>
2023-09-11 12:40:46 -0700

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
+    if matrix:
+        columns = len(matrix[0])
+    else:
+        columns = 0
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for x, element in enumerate(row):
+            if x == columns - 1: 
+                print("{:d}".format(element), end="")
+            else:
+                print("{:d}".format(element), end=" ")
         print("")
