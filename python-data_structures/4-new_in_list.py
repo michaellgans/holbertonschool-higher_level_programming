f2c4969fc056d48ab4b2d23561f24f4aa1c1bcca
michaellgans <michaellgans@hotmail.com>
2023-09-11 10:37:42 -0700

    creates task 4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx not in range(len(my_list)):
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
