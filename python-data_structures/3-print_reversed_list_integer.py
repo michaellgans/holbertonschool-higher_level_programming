366286c7f48fa3050dbae83102de4ca402e0b428
michaellgans <michaellgans@hotmail.com>
2023-09-11 09:59:54 -0700

    Edge Case Test

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print(my_list)
-    for x in reversed(my_list):
-        print("{:d}".format(x))
+    if type(my_list) is list:
+        for x in reversed(my_list):
+            print("{:d}".format(x))
