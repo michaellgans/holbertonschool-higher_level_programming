0823b5ab55a3ee6712ee12036125c59834ac8882
michaellgans <michaellgans@hotmail.com>
2023-09-11 15:04:16 -0700

    test

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,9 +3,6 @@
 def multiple_returns(sentence):
     
     if sentence == "":
-        first = None
+        return 0, None
     else:
-        first = sentence[0]
-    length = len(sentence)
-
-    return length, first
+        return len(sentence), sentence[0]
