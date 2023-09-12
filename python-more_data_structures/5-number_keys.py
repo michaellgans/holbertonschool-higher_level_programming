f410db2251dc0e08eb30cb99d84bda08aaf4f980
michaellgans <michaellgans@hotmail.com>
2023-09-11 21:40:41 -0700

    test

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def number_keys(a_dictionary):
-    return (0 if not isinstance(a_dictionary, dict)
-    else len(a_dictionary) + sum(number_keys(val) for val in a_dictionary.values()))
-    res = number_keys(a_dictionary)
+def number_keys(a_dic):
+    return (0 if not isinstance(a_dic, dict)
+        else len(a_dic) + sum(number_keys(val) for val in a_dic.values()))
+    res = number_keys(a_dic)
     return res
