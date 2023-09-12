50a930840509e83d541b52b25d8cc6186e870c67
michaellgans <michaellgans@hotmail.com>
2023-09-12 13:41:12 -0700

    Creates task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list.copy()
-    for x in range(len(new_list)):
-        new_list[x] = new_list[x] * number
-    return new_list
+    result = map(lambda x: x * number, my_list)
+    return list(result)
