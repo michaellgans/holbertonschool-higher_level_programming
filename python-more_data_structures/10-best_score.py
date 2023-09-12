bd54cd8bfae2a57108f34aa185ad75dd2fa685a4
michaellgans <michaellgans@hotmail.com>
2023-09-12 13:13:50 -0700

    test

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,4 +4,6 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     max_key = max(a_dictionary.keys())
+    if max_key is None:
+        return None
     return max_key
