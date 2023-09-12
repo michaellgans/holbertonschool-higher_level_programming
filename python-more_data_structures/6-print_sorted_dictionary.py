1e2767190080bde1c305fbd24c03a488b70e0aea
michaellgans <michaellgans@hotmail.com>
2023-09-12 12:18:23 -0700

    Creates task 7

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     keys = list(a_dictionary.keys())
     keys.sort()  # sorts keys list
     # creates new dictionary
