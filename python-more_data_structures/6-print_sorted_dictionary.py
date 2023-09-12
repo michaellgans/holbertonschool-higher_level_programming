7360b9bd96d413c6c2cbb0f740f9a1c3f0c7d324
michaellgans <michaellgans@hotmail.com>
2023-09-12 12:01:36 -0700

    Creates Task 6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()  # sorts keys list
+    # creates new dictionary
+    sorted_dict = {x: a_dictionary[x] for x in keys}
+
+    for key in sorted_dict:
+        print("{}: {}".format(key, sorted_dict[key]))
--- python-more_data_structures/README.md
+++ python-more_data_structures/README.md
@@ -3,3 +3,6 @@
 ## FYSK
 - set()
 - intersection, union, difference, symmetric difference
+- keys() prints the keys in a dictionary
+- sort()
+
