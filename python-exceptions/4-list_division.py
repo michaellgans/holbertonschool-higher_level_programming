25f094a3f606138fe90e40710bbc13c251af7785
michaellgans <michaellgans@hotmail.com>
2023-09-12 17:43:57 -0700

    once more with feeling

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("{}".format("out of range"))
+            result = 0
         finally:
             new_list.append(result)
     return new_list
