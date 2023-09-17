809578cfab4897efbcc8c9f49d38737cb2c00e35
michaellgans <michaellgans@hotmail.com>
2023-09-17 14:24:50 -0700

    Creates task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines a class """
+
+
+class Square:
+    """ Names the class Square """
+    def __init__(self, size=0):
+        """ Initiazlies the data """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
