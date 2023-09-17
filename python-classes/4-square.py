7351228acde5f063f5d54e9afc7ef1f7601554b6
michaellgans <michaellgans@hotmail.com>
2023-09-17 16:40:47 -0700

    Creates task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Defines a class """
+
+
+class Square:
+    """ Names the class Square """
+    def __init__(self, size=0):
+        """ Initiazlies the data """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ Calculates the area of the square
+        Returns:
+            The area of a rectangle
+
+        """
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        """ Retrieves size of square """
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = value  # resets size to value
