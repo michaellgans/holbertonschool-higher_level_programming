26b69e2f4d1f0eac4a2adcc41f8e9fbf13221aab
michaellgans <michaellgans@hotmail.com>
2023-09-18 14:37:21 -0700

    reverting

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,12 +13,10 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        if not isinstance(position, tuple) or \
-            len(position) != 2 or \
-            position[1] < 0 or \
-            position[0] < 0 or \
-            not isinstance(position[1], int) or \
-            not isinstance(position[0], int):
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if position[1] < 0 or position[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__size = size
@@ -57,12 +55,13 @@ class Square:
     @position.setter
     def position(self, value):
 
-        if not isinstance(self.__position, tuple) or \
-            len(self.__position) != 2 or \
-            self.__position[1] < 0 or \
-            self.__position[0] < 0 or \
-            not isinstance(self.__position[1], int) or \
-            not isinstance(self.__position[0], int):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if value[1] < 0 or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if not isinstance(value[1], int) or not isinstance(value[0], int):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value  # resets position to value
