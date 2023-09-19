a6c52197b53fff6b2b120c2a827a4583936608e9
michaellgans <michaellgans@hotmail.com>
2023-09-19 15:28:32 -0700

    Creats task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Prints out first and last name """
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        Args:
+            first_name (string)
+            last_name (string)
+
+        Raises:
+            TypeError: if first and last name aren't strings
+        """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
+    print("My name is {} {}".format(first_name, last_name))
