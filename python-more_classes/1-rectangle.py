c71ae8e54d390451fe9b3a86451575717e952b5e
michaellgans <michaellgans@hotmail.com>
2023-09-18 17:45:35 -0700

    Creates task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Creates a class named Rectangle """
+
+
+class Rectangle:
+    """ Defines the rectangle """
+    def __init__(self, width=0, height=0):
+        """ Initializes the Data """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >=0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
