f9aabf1ff81b3185cc2c0dcfeffdf51a8cd7e7f8
michaellgans <michaellgans@hotmail.com>
2023-09-18 19:00:16 -0700

    Creates task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" Creates a class named Rectangle """
+
+
+class Rectangle:
+    """ Initializes the Data for Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """
+            Args:
+                width (int): width of the rectangle
+                height (int): height of the rectangle
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ Returns the width of the rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width of the rectangle """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ Returns the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height of the rectangle """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    
+    def area(self):
+        """ Returns the area of Rectangle """
+        return self.__height * self.__width
+    
+    def perimeter(self):
+        """ Returns the perimeter of Rectangle """
+        if self.__width == 0 and self.__height == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.__width * 2)
