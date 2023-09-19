12dd838faaf3581e3e72fff3e4b838d66ab6c78c
michaellgans <michaellgans@hotmail.com>
2023-09-18 18:12:01 -0700

    type casts value

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -20,7 +20,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = value
+            self.__width = int(value)
 
     @property
     def height(self):
@@ -33,4 +33,4 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = value
+            self.__height = int(value)
