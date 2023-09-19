7972d4f8cf43eb4e23f59af523d66a0765f0ae98
michaellgans <michaellgans@hotmail.com>
2023-09-18 18:40:06 -0700

    not private?

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
                 width (int): width of the rectangle
                 height (int): height of the rectangle
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
