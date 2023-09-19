f30adc116b46161909ca21a717aee0efafc845b4
michaellgans <michaellgans@hotmail.com>
2023-09-18 17:54:08 -0700

    changing order of if logic

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,11 +10,11 @@ class Rectangle:
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
 
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-
         if width < 0:
             raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
 
         if height < 0:
             raise ValueError("height must be >= 0")
