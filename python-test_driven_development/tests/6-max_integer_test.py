0d97030bb2905989b89220a58c3e770adc6e90af
michaellgans <michaellgans@hotmail.com>
2023-09-19 17:20:44 -0700

    adds tests to task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,8 +6,37 @@ max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """ Class for testing max integer """
-    def test_max_integer(self):
-        """ All integers are positive """
+    def test_max_end(self):
+        """ Max number at the end """
         test_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(max_integer(test_list), 9)
 
+    def test_max_beginning(self):
+        """ Max number at the beginning """
+        test_list = [9, 4, 1]
+        self.assertEqual(max_integer(test_list), 9)
+
+    def test_max_middle(self):
+        """ Max number in the middle """
+        test_list = [4, 9, 1]
+        self.assertEqual(max_integer(test_list), 9)
+
+    def test_one_negative(self):
+        """ One negative number in list """
+        test_list = [-1, 3, 9]
+        self.assertEqual(max_integer(test_list), 9)
+
+    def test_all_negative(self):
+        """ All negative numbers """
+        test_list = [-1, -2, -3]
+        self.assertEqual(max_integer(test_list), -1)
+
+    def test_short_lsit(self):
+        """ Just one number in list """
+        test_list = [1]
+        self.assertEqual(max_integer(test_list), 1)
+
+    def test_empty_list(self):
+        """ Empty list """
+        test_list = []
+        self.assertEqual(max_integer(test_list), None)
