3179897a8ff7cdcad06b5b0dcb014f51188763b1
michaellgans <michaellgans@hotmail.com>
2023-09-19 18:14:20 -0700

    test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,19 +16,10 @@ def text_indentation(text):
     new_text = ""
     delimiters = ".?:"
 
-    flag = False
-
     for x in range(len(text)):
         if text[x] in delimiters:
             new_text += text[x]
-            flag = True
-        elif flag and text[x] == " ":
-            continue
-        elif flag:
             new_text += "\n\n"
-            new_text += text[x]
-            flag = False
         else:
             new_text += text[x]
     print(new_text, end="")
-    print("")
