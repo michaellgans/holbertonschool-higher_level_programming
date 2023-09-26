fd289ff91618761178bd1f335e42327a9dcd8945
michaellgans <michaellgans@hotmail.com>
2023-09-26 09:59:38 -0700

    adds documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" reads a file """
+""" Task 0 """
 
 
 def read_file(filename=""):
+    """ Reads a file with a with statement """
     with open("my_file_0.txt", "r") as file:
         file_contents = file.read()
         print(file_contents, end="")
