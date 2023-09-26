cd5567990430f1fe4f840ce0de5ca90c23930c89
michaellgans <michaellgans@hotmail.com>
2023-09-25 20:36:05 -0700

    creates task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates a class """
+
+
+class BaseGeometry:
+    """ Empty Class """
+    def area(self):
+        """ Area of something """
+        raise Exception("area() is not implemented")
