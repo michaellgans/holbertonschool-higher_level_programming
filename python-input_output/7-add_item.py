2fa8b56a93f3d8aa8b830503602c8e3fe2526dc8
michaellgans <michaellgans@hotmail.com>
2023-09-26 16:55:21 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 my_list = []
 
-if ox.path.exists("add_item.json"):
+if os.path.exists("add_item.json"):
     my_list = load_from_json_file("add_item.json")
 else:
     my_list = []
