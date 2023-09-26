ae2578cefbf303363144ffc3c0af58f7f37e87b8
michaellgans <michaellgans@hotmail.com>
2023-09-26 10:03:42 -0700

    utf8 added

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Reads a file with a with statement """
-    with open("my_file_0.txt", "r") as file:
+    with open("my_file_0.txt", encoding="utf-8") as file:
         file_contents = file.read()
         print(file_contents, end="")
