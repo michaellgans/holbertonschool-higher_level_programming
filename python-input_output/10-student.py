363ca0f41de19f1f6d1dcad63fe02559ba5da87f
michaellgans <michaellgans@hotmail.com>
2023-09-26 17:26:02 -0700

    task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Project 3, Task 9 """
+
+
+class Student:
+    """ Defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dict description """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
