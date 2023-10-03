8ff720c8d8ea8451a0a785325f02e1de4c1b3bf8
michaellgans <michaellgans@hotmail.com>
2023-10-03 14:02:44 -0700

    Adds a dict representation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -126,3 +126,13 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Dict representation of Rectangle """
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
