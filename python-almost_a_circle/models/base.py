54867eeae0c347300d62e88996e1fb1f980b4b9b
michaellgans <michaellgans@hotmail.com>
2023-10-03 18:52:13 -0700

    updates pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -47,6 +47,18 @@ class Base:
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
-    
+
     @classmethod
-    
+    def create(cls, **dictionary):
+        """Creates an instance with all attributes set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """return list of instances"""
+        file = cls.__name__ + ".json"
