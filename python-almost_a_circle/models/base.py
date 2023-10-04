7bcefd4f92fd9541c732aefe9bebdf3111d7d73e
michaellgans <michaellgans@hotmail.com>
2023-10-03 18:28:47 -0700

    Creates task 14 and 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Write the first class """
+import json
 
 
 class Base:
@@ -20,3 +21,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns JSON string rep of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON str rep of list_objs"""
+        file = cls.__name__ + ".json"
+        with open(file, "w") as fd:
+            if list_objs is None:
+                fd.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                fd.write(Base.to_json_string(list_dicts))
