49ebda6f15a57a33f50ee8bb326c91f8d7b54105
michaellgans <michaellgans@Victoria>
2023-10-22 14:19:46 -0500

    Creates 11

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- list score and name from second_table ordered by score
+-- where score is greater than or equal to 10
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
