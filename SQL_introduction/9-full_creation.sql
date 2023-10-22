a030b3714572d5a0b64445c8e54306332d794e6c
michaellgans <michaellgans@Victoria>
2023-10-22 14:06:10 -0500

    Edits 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE second_table (
 
 INSERT INTO second_table (id, name, score)
 VALUES 
-("1", "John", "10"),
-("2", "Alex", "3"),
-("3", "Bob", "14"),
-("4", "George", "8");
+    ("1", "John", "10"),
+    ("2", "Alex", "3"),
+    ("3", "Bob", "14"),
+    ("4", "George", "8");
