b21a31a0073ad4febf376e926ffe413c9f6e8b29
michaellgans <michaellgans@Victoria>
2023-10-23 13:35:44 -0500

    edits 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,6 +5,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
-    UNIQUE (id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
