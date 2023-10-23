a3ec8db66bfbe340994e01f45221ce347a95fb53
michaellgans <michaellgans@Victoria>
2023-10-23 13:34:15 -0500

    Creates task 7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Foreign Keys
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id INT NOT NULL 
+    FOREIGN KEY (state_id) REFERENCES states(id)
+    UNIQUE (id)
+);
