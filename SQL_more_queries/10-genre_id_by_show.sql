fe3d31423d1ab79290ee9928d71d2feb2b191bdc
michaellgans <michaellgans@Victoria>
2023-10-23 17:04:32 -0500

    Create 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- Left join
+-- SELECT columns
+-- FROM table1
+-- TYPE JOIN table2 ON table1.column = table2.column
+
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_shows_genres ON tv_shows.genre_id = tv_shows_genres.id;
