6d1764ec22b39d05e96f7a1c2ff239a5dcf9977d
michaellgans <michaellgans@Victoria>
2023-10-24 10:27:33 -0500

    Edits 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT DISTINCT tv_genres.name AS genre,
 COUNT (tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.show_id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
