15ed7ed19c172c7cd0c03916f5b5c2732f05479e
michaellgans <michaellgans@Victoria>
2023-10-24 09:50:33 -0500

    Edits 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,4 +5,5 @@
 
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id;
+JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
