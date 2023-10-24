aaa98a04c1179d78dd300a8e3a4ea7b9019382d1
michaellgans <michaellgans@Victoria>
2023-10-24 09:54:51 -0500

    Edits 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- FROM table1
 -- TYPE JOIN table2 ON table1.column = table2.column
 
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
