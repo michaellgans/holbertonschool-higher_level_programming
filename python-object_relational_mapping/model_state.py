b4353c75151d46ebc44b8879d1ea4875cdf0cc76
michaellgans <michaellgans@Victoria>
2023-10-24 12:46:41 -0500

    Creates 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Task 6 """
+import Column from sqlalchemy
+import Integer from sqlalchemy
+import String from sqlalchemy
+import declarative_base from sqlalchemy.ext.declarative
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ Class for state, location for DB """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True,
+                nullable=False,
+                autoimcrement="auto",
+                unique=True)
+    name = Column(String(128), nullable=False)
