fcff2998e7a2a75da006459174df1b8333ab3f32
michaellgans <michaellgans@Victoria>
2023-10-24 11:57:13 -0500

    Edits 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ def all_states():
 
     """ Connect to MySQL """
     DB = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], DB=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     """ Set a cursor """
     cursor = DB.cursor()
