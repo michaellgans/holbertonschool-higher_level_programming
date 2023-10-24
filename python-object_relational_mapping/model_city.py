1e4e4c61ababe44b59d693253fe3f1564e7deefa
michaellgans <michaellgans@Victoria>
2023-10-24 14:16:17 -0500

    Edits model_city again

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,12 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """ Class for city, location for DB """
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True,
-                nullable=False,
-                autoincrement=True,
-                unique=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
