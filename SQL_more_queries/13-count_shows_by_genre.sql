52c1c4735507652b4f2c74283359739505451043
michaellgans <michaellgans@Victoria>
2023-10-24 10:26:09 -0500

    Edits 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ COUNT (tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.show_id = tv_show_genres.genre_id
 GROUP BY genre
-ORDER BY number_of_shows DESC
+ORDER BY number_of_shows DESC;
