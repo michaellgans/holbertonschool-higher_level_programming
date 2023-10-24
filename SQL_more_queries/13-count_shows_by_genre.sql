e847d9abda1e60d8a3c5b11fbca7cf23e6e46822
michaellgans <michaellgans@Victoria>
2023-10-24 10:30:13 -0500

    Edits 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all genres and displays the count of shows per genre
-SELECT DISTINCT tv_genres.name AS genre,
+SELECT DISTINCT tv_shows.name AS genre,
 COUNT (tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
-ORDER BY number_of_shows DESC
+ORDER BY number_of_shows DESC;
