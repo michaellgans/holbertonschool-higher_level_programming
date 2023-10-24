a55430dbdb1e0ed5697d2916a85cd1f1b4747696
michaellgans <michaellgans@Victoria>
2023-10-24 10:55:02 -0500

    Edits 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 --Lists all genres of the show Dexter...?
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
