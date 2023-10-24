dd42ad4650cd210f57f96a8ed5dded996e7bb10e
michaellgans <michaellgans@Victoria>
2023-10-24 11:45:50 -0500

    Creates 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+import sys
+import MySQLdb
+
+
+def all_states():
+    """ Lists all states from the database """
+
+    """ Connect to MySQL """
+    DB = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1],
+                         passwd=sys.argv[2], DB=sys.argv[3])
+
+    """ Set a cursor """
+    cursor = DB.cursor()
+
+    """ Inject SQL Query """
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    """ Grab the results of the Query """
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    """ Close the connection """
+    cursor.close()
+    DB.close()
+
+
+if __name__ == "__main__":
+    all_states()
