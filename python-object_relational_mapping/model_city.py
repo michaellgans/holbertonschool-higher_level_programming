30d9c3c4e95d974137282667e8edf2414b0a291e
michaellgans <michaellgans@Victoria>
2023-10-24 13:59:34 -0500

    Edits model_city again

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Task 14 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class State(Base):
     """ Class for city, location for DB """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"))
