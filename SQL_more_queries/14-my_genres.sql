6e14279359800f1154df9fa5d7de2c7b0d6772d7
michaellgans <michaellgans@Victoria>
2023-10-24 10:45:34 -0500

    Edits 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.shows_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = "Dexter"
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
