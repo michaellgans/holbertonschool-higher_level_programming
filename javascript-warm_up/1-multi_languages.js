a31c254adff68e5a08f1c09944965ad77c4a94ca
michaellgans <michaellgans@Victoria>
2023-11-27 11:53:06 -0600

    Three console log commands instead of just one

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun\n', 'Python is cool\n', 'JavaScript is amazing');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
