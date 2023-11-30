686bd2fd4d5f4f41535b45fbe4aab68dd3334b43
michaellgans <michaellgans@Victoria>
2023-11-30 05:27:44 -0600

    Trying new logic

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 }
 
