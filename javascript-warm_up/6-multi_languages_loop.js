3d79612a28633164811484ad8df365b8454d4a7c
michaellgans <michaellgans@Victoria>
2023-11-30 01:55:21 -0600

    Cammel Case and Const instead of Let

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let print_me = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const printMe = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let x = 0; x <= 2; x++) {
-  console.log(print_me[x]);
+  console.log(printMe[x]);
 }
