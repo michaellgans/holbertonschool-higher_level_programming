cae0a42563999a7cb2dee3451532aa61b17ba944
michaellgans <michaellgans@Victoria>
2023-11-30 02:32:35 -0600

    Creates task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = Number(process.argv[2]);
+
+if (!size || isNaN(size)) {
+  console.log('Missing size');
+}
+
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y < size; y++) {
+    process.stdout.write('x');
+  }
+  console.log();
+}
