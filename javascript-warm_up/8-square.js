a72e68bf1dc843e2339482290c257ba182ec769f
michaellgans <michaellgans@Victoria>
2023-11-30 02:34:25 -0600

    capital X

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -8,7 +8,7 @@ if (!size || isNaN(size)) {
 
 for (let x = 0; x < size; x++) {
   for (let y = 0; y < size; y++) {
-    process.stdout.write('x');
+    process.stdout.write('X');
   }
   console.log();
 }
