702be814aab8216a0eb79b7bf6376f3ac4f7ae91
michaellgans <michaellgans@Victoria>
2023-12-04 00:54:43 -0600

    added response though it's unused?

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, function (err, body) {
+request.get(url, function (err, response, body) {
   if (err) {
     console.error(err);
   } else {
